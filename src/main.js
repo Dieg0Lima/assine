78197e125eb1b2611d37c43120d0da1c77d3190b
Diego Costa Lima <diego.lima@tote.local>
2023-09-28 09:38:44 -0300

    GTM-Vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import VueGtm from '@gtm-support/vue-gtm';
+
+const app = createApp(App);
+
+app.use(VueGtm, {
+   id: 'GTM-WBR3L4QD' ,
+    vueRouter: router,
+});
 
 createApp(App).use(store).use(router).mount('#app')
